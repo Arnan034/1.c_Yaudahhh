3a38799e73848335f643ae40a52f343e1aab2497
yuwan121 <117898042+yuwan121@users.noreply.github.com>
2023-05-10 00:20:22 +0700

    UPDATE FINAL
    UPDATE FINAL

--- tree.h
+++ tree.h
@@ -15,17 +15,21 @@ typedef struct calcTree{
     struct calcTree * rChild;//Right child 
 };
 
-double count(struct calcTree *root);
+double count(struct calcTree *root); 
 //Function untuk menghitung total dari proses proses dalam tree
-double inspectExpression(char mathExpression[], int firstIndex, int lastIndex);
+double inspectExpression(char mathExpression[], int firstIndex, int lastIndex); 
 //Function untuk memeriksa apakah inputan mengandung operator atau hanya angka.
-struct calcTree * createTree(char mathExpression[], int firstIndex, int lastIndex);
+struct calcTree * createTree(char mathExpression[], int firstIndex, int lastIndex); 
 //Function untuk membuat tree
 char isOperator(struct calcTree *root);
 //Function untuk mengecek operator atau bukan
 double round_double(double number, int decimal_places);
 //Function untuk membulatkan hasil perhitungan
-bool isOperasi(char oper);
+bool isOperasi(char oper); 
 //Mengecek kesesuaian suatu operator
-bool isValid(char input[]);
+bool isValid(char input[]); 
 //Function ini untuk mengecek apakah operasi yang di inputkan valid atau tidak.
+void infixToPostfix(struct calcTree *root);
+//Mengubah ekspresi matematika infix menjadi postfix
+void infixToPrefix(struct calcTree *root);
+//Mengubah ekspresi matematika infix menjadi pretfix
